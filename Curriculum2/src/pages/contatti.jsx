965d982790cc9200c2bda8b6eab84d326d84bfc1
AlessandroAmen <xela9827@gmail.com>
2025-04-26 18:19:41 +0200

    aggiunti contatti

--- Curriculum2/src/pages/contatti.jsx
+++ Curriculum2/src/pages/contatti.jsx
@@ -1,11 +1,16 @@
 import Navbar from '../components/Navbar.jsx';
+import './contatti.css';
 
 function Contatti() {
   return (
-    <>
+    <div className="contatti-container">
       <h1>Contattami</h1>
-      <p>Puoi contattarmi via email all'indirizzo: example@example.com</p>
-    </>
+      <p>Puoi contattarmi tramite:</p>
+      <ul>
+        <li>Email: <a href="mailto:xela9827@gmail.com">xela9827@gmail.com</a></li>
+        <li>Telefono: <a href="tel:+393923177199">+39 392 317 7199</a></li>
+      </ul>
+    </div>
   );
 }
 
